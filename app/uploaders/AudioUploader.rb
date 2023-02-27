db46fb9bfb725715beceda1f55ab34810c3701b3
Alexander Abramov <omgout200@gmail.com>
2023-02-27 15:05:15 +0700

    setup shrine gem

---
+++ app/uploaders/AudioUploader.rb
@@ -0,0 +1,3 @@
+class AudioUploader < Shrine
+  # plugins and uploading logic
+end
---
+++ db/migrate/20230227075813_add_file_to_posts.rb
@@ -0,0 +1,5 @@
+class AddFileToPosts < ActiveRecord::Migration[7.0]
+  def change
+    add_column :posts, :audio_data, :text
+  end
+end
