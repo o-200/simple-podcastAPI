c96867f1a646ec257f3ac6eb68ff75d0f1b5707b
Alexander Abramov <omgout200@gmail.com>
2023-02-28 14:02:24 +0700

    setup simplecov and rspec gems

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Post.destroy_all
+
+4.times do
+  uploader = AudioUploader.new(:store)
+  file = File.new(Rails.root.join('app/assets/images/seed/test.png'))
+
+
+  post = Post.create!(
+    title: 'Faker::Commerce.product_name',
+    body: 'Faker::Markdown.emphasis',
+    audio: uploader.upload(file)
+  )
+  puts post.title
+end
